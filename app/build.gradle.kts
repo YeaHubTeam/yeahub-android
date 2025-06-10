b1e3212ce15873f0a14ebef13122a4e7bca14606
AlexV <fossercss@gmail.com>
2025-06-10 23:41:50 +0300

    added lifecycle-viewmodel-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    //ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
