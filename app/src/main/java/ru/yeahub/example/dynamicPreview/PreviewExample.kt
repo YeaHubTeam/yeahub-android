b7b6cbf4e5f96029753f97d1a702d7db7335f237
cools <cool.strekalov@mail.ru>
2025-06-10 17:46:30 +0300

    Added the enum class Added a modifier to the function parameter Removed the style from the text

--- app/src/main/java/ru/yeahub/example/dynamicPreview/PreviewExample.kt
+++ app/src/main/java/ru/yeahub/example/dynamicPreview/PreviewExample.kt
@@ -13,12 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
+
 
 @Preview(
     name = "Dynamic Preview",
@@ -37,41 +38,54 @@ fun DynamicPreview(@PreviewParameter(NumbersPreviewProvider::class) numbers: Int
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ScreenCount(viewModel: MyViewModel) {
-    val standardWidth = 360.dp // Ширина для стандартных устройств
-    val standardHeight = 640.dp // Высота для стандартных устройств
+fun ScreenCount(
+    modifier: Modifier = Modifier,
+    textModifier: Modifier = Modifier,
+    buttonModifier: Modifier = Modifier,
+    textOnButtonModifier: Modifier = Modifier,
+    viewModel: MyViewModel,
+    screenSize: ScreenSize = ScreenSize.STANDARD,
+) {
     val textState = viewModel.textState.value
 
+    val standardWidth = screenSize.width
+    val standardHeight = screenSize.height
+
     Column(
-        modifier = Modifier
+        modifier = modifier
             .width(standardWidth)
             .height(standardHeight),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            modifier = Modifier.padding(bottom = 30.dp), // Отступ снизу
+            modifier = textModifier.padding(bottom = 30.dp),
             text = "Current Count: $textState",
-            style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            fontSize= 30.sp
         )
         Spacer(modifier = Modifier.height(30.dp))
         Button(
-            modifier = Modifier
+            modifier = buttonModifier
                 .width(200.dp)
                 .height(60.dp),
             onClick = { viewModel.increment() },
         ) {
             Text(
+                modifier = textOnButtonModifier,
                 style = TextStyle(fontSize = 18.sp),
                 text = "Count++"
             )
         }
     }
+
 }
 
 class NumbersPreviewProvider : PreviewParameterProvider<Int> {
     override val values: Sequence<Int> = sequenceOf(22, 45, 6666, 123563)
 }
 
+enum class ScreenSize(val width: Dp, val height: Dp) {
+    STANDARD(360.dp, 640.dp),
+}
 
 
