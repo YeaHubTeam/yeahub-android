dafd5c0f1ff50afd84e3961ba83b512e85e73c01
Anastasia <nastya19730211@mail.ru>
2025-06-12 14:05:07 +0300

    delete default params

--- core/network-impl/src/main/java/ru/yeahub/network_impl/ApiService.kt
+++ core/network-impl/src/main/java/ru/yeahub/network_impl/ApiService.kt
@@ -1,10 +1,10 @@
-package com.example.network_impl
+package ru.yeahub.network_impl
 
-import com.example.network_api.models.QuestionDto
-import com.example.network_api.models.QuestionResponseDto
-import com.example.network_api.models.SkillsResponseDto
-import com.example.network_api.models.SpecializationDto
-import com.example.network_api.models.SpecializationResponseDto
+import ru.yeahub.network_api.models.QuestionDto
+import ru.yeahub.network_api.models.QuestionResponseDto
+import ru.yeahub.network_api.models.SkillsResponseDto
+import ru.yeahub.network_api.models.SpecializationDto
+import ru.yeahub.network_api.models.SpecializationResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,19 +13,19 @@ interface ApiService {
 
     @GET("questions/public-questions")
     suspend fun getQuestions(
-        @Query("page") page: Int = 1,
-        @Query("limit") limit: Int = 10,
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
         @Query("title") title: String? = null,
         @Query("titleOrDescription") titleOrDescription: String? = null,
         @Query("skills") skills: List<String>? = null,
-        @Query("skillFilterMode") skillFilterMode: String? = "ALL",
+        @Query("skillFilterMode") skillFilterMode: String? = null,
         @Query("complexity") complexity: List<Int>? = null,
         @Query("collection") collection: Int? = null,
         @Query("rate") rate: List<Int>? = null,
         @Query("keywords") keywords: List<String>? = null,
         @Query("specialization") specialization: Int? = null,
         @Query("orderBy") orderBy: String? = null,
-        @Query("order") order: String? = "ASC",
+        @Query("order") order: String? = null,
         @Query("random") random: Boolean? = null
     ): QuestionResponseDto
 
@@ -36,15 +36,15 @@ interface ApiService {
 
     @GET("skills")
     suspend fun getSkills(
-        @Query("page") page: Int = 1,
-        @Query("limit") limit: Int = 10,
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
         @Query("specializations") specializations: List<Int>? = null
     ): SkillsResponseDto
 
     @GET("specializations")
     suspend fun getAllSpecializations(
-        @Query("page") page: Int = 1,
-        @Query("limit") limit: Int = 10
+        @Query("page") page: Int,
+        @Query("limit") limit: Int
     ): SpecializationResponseDto
 
     @GET("specializations/{id}")
