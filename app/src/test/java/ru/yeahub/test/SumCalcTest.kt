e872440d1a9d44932aa1f075d65b04c4759e5fcb
alexxelo <a.alexxelo@gmail.com>
2025-06-13 18:50:56 +0700

    Исправлен SumCalcTest: теперь содержит внутри SumCalcTestCase и SumCalcArgumentsProvider

--- app/src/test/java/ru/yeahub/test/SumCalcTest.kt
+++ app/src/test/java/ru/yeahub/test/SumCalcTest.kt
@@ -7,11 +7,27 @@ import org.junit.jupiter.params.provider.ArgumentsSource
 
 class SumCalcTest {
 
+    data class SumCalcTestCase(
+        val dataToTest: Pair<Int, Int>,
+        val expectedResult: Int
+    )
+
+    class SumCalcArgumentsProvider : TestArgumentsProvider<SumCalcTestCase>() {
+
+        override fun testCases(): List<SumCalcTestCase> = listOf(
+            SumCalcTestCase(Pair(1, 2), 3),
+            SumCalcTestCase(Pair(5, 2), 7),
+            SumCalcTestCase(Pair(0, 0), 0),
+            SumCalcTestCase(Pair(1, -1), 0),
+            SumCalcTestCase(Pair(Int.MAX_VALUE, 0), Int.MAX_VALUE),
+            SumCalcTestCase(Pair(Int.MIN_VALUE, 0), Int.MIN_VALUE)
+        )
+    }
+
     @ParameterizedTest
     @ArgumentsSource(SumCalcArgumentsProvider::class)
-    fun `should calculate sum correctly`(testCase: TestCase<Pair<Int, Int>, Int>) {
+    fun `should calculate sum correctly`(testCase: SumCalcTestCase) {
         val result = SumCalc().sum(testCase.dataToTest.first, testCase.dataToTest.second)
         assertEquals(testCase.expectedResult, result)
     }
-
 }
--- app/src/test/java/ru/yeahub/test/TestCase.kt
+++
@@ -1,7 +0,0 @@
-package ru.yeahub.test
-
-
-data class TestCase<T, R>(
-    val dataToTest: T,
-    val expectedResult: R
-)
