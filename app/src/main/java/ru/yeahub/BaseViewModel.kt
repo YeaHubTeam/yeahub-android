2a29ecf9817a28c57aed8dfcb11582e2b007bcfb
AlexV <fossercss@gmail.com>
2025-06-13 18:43:35 +0300

    исправлена реализация BaseViewModel

--- app/src/main/java/ru/yeahub/BaseViewModel.kt
+++ app/src/main/java/ru/yeahub/BaseViewModel.kt
@@ -1,28 +1,22 @@
 package ru.yeahub
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
+import timber.log.Timber
 
 abstract class BaseViewModel : ViewModel() {
 
-    protected val defaultCEH: CoroutineExceptionHandler
+    private val defaultCEH: CoroutineExceptionHandler
         get() = CoroutineExceptionHandler { _, throwable ->
             onDefaultCEH(throwable)
         }
 
     protected open fun onDefaultCEH(throwable: Throwable) {
-        val tag = this.javaClass.simpleName
-        Log.e(tag, "Unhandled exception: ${throwable.message}", throwable)
+        Timber.e(throwable, "Unhandled exception in ${this.javaClass.simpleName}")
     }
 
-    protected fun runCoroutine(
-        context: CoroutineContext = EmptyCoroutineContext,
-        block: suspend CoroutineScope.() -> Unit,
-    ) = viewModelScope.launch(context) { block() }
+    fun CoroutineScope.withDefaultCEH(): CoroutineScope {
+        return CoroutineScope(this.coroutineContext + defaultCEH)
+    }
 }
