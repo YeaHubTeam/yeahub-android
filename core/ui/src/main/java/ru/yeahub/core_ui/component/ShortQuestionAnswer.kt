44d502cad4d742eeca577622516be8a37662a3b1
Valentine Ershov <valike.ershoff@gmail.com>
2025-07-10 17:33:05 +0300

    [ANDROID-25] Buttons static previews

--- core/ui/src/main/java/ru/yeahub/core_ui/component/ShortQuestionAnswer.kt
+++ core/ui/src/main/java/ru/yeahub/core_ui/component/ShortQuestionAnswer.kt
@@ -12,8 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.yeahub.core_ui.example.staticPreview.StaticPreview
 import ru.yeahub.core_ui.theme.Theme
 import ru.yeahub.ui.R
 
@@ -51,7 +51,7 @@ fun ShortQuestionAnswer(
     }
 }
 
-@Preview
+@StaticPreview
 @Composable
 fun ShortQuestionAnswerPreview() {
     ShortQuestionAnswer(
--- core/ui/src/main/java/ru/yeahub/core_ui/example/staticPreview/StaticPreview.kt
+++ core/ui/src/main/java/ru/yeahub/core_ui/example/staticPreview/StaticPreview.kt
@@ -4,28 +4,23 @@ import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
-    name = "Default theme preview",
+    name = "Normal screen (phone) preview",
     group = "StaticPreviews",
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     showBackground = true,
+    device = "id:pixel_9",
 )
-@Preview(
-    name = "Dark theme preview",
-    group = "StaticPreviews",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+// Пока не будет темной темы - неактивно
+//@Preview(
+//    name = "Dark theme preview",
+//    group = "StaticPreviews",
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//)
 @Preview(
     name = "Wide screen (tablet) preview",
     group = "StaticPreviews",
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     showBackground = true,
     device = "id:Nexus 9",
-)
-@Preview(
-    name = "Normal screen (phone) preview",
-    group = "StaticPreviews",
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    showBackground = true,
-    device = "id:pixel_9",
 )
 annotation class StaticPreview
