8d2118bdd039f6e733aee96d99c8d706252a34b5
alexxelo <71372950+alexxelo@users.noreply.github.com>
2025-07-17 11:42:56 +0300

    Android 29 ExtendedQuestion (#27)
    * ANDROID-29 В HideQuestion добавлена часть скрытого вопроа ExtendedQuestion c анимациями

--- core/ui/src/main/java/ru/yeahub/core_ui/theme/Typography.kt
+++ core/ui/src/main/java/ru/yeahub/core_ui/theme/Typography.kt
@@ -39,7 +39,7 @@ data class Typography(
     val head4: TextStyle = TextStyle(
         fontFamily = manrope,
         fontWeight = FontWeight.Medium,
-        fontSize = 18.sp,
+        fontSize = 20.sp,
         lineHeight = 23.sp,
     ),
     //Body
