991c5651e6179702877b30472fe42bd6f9ffa363
Daniil-Tomaylyi <daniiltomajly@gmail.com>
2025-07-24 16:06:21 +0300

    ANDROID-48 mock данные заменены реальным вызовом usecase в viewmodel

--- feature/detail-question/impl/src/main/java/com/example/impl/presentation/mapper/DetailQuestionScreenMapper.kt
+++ feature/detail-question/impl/src/main/java/com/example/impl/presentation/mapper/DetailQuestionScreenMapper.kt
@@ -1,10 +1,93 @@
 package com.example.impl.presentation.mapper
 
-import com.example.impl.presentation.view.DetailQuestionState
-import com.example.impl.presentation.viewmodel.publicQuestion
+import com.example.impl.domain.models.GuruEntity
+import com.example.impl.domain.models.NestedSkillEntity
+import com.example.impl.domain.models.NestedSpecializationEntity
+import com.example.impl.domain.models.NestedUserReferenceEntity
+import com.example.impl.domain.models.PublicQuestionEntity
+import com.example.impl.presentation.state.DetailQuestionState
+import com.example.impl.presentation.state.GuruVO
+import com.example.impl.presentation.state.NestedSkillVO
+import com.example.impl.presentation.state.NestedSpecializationVO
+import com.example.impl.presentation.state.NestedUserReferenceVO
 
 class DetailQuestionScreenMapper {
-    fun getScreenState(): DetailQuestionState {
-        return DetailQuestionState.Success(publicQuestion)
+    fun getScreenState(publicQuestionEntity: PublicQuestionEntity): DetailQuestionState {
+        return DetailQuestionState.Success(
+            DetailQuestionState.Success.PublicQuestionVO(
+                id = publicQuestionEntity.id,
+                title = publicQuestionEntity.title,
+                description = publicQuestionEntity.description,
+                code = publicQuestionEntity.code,
+                imageSrc = publicQuestionEntity.imageSrc,
+                longAnswer = publicQuestionEntity.longAnswer,
+                shortAnswer = publicQuestionEntity.shortAnswer,
+                keywords = publicQuestionEntity.keywords,
+                status = publicQuestionEntity.status,
+                rate = publicQuestionEntity.rate,
+                complexity = publicQuestionEntity.complexity,
+                createdById = publicQuestionEntity.createdById,
+                updatedById = publicQuestionEntity.updatedById,
+                questionSpecializations = publicQuestionEntity.questionSpecializations.map {
+                    getQuestionSpecializationsToVO(
+                        it
+                    )
+                },
+                questionSkills = publicQuestionEntity.questionSkills.map {
+                    getQuestionSkillsToVO(
+                        it
+                    )
+                },
+                createdAt = publicQuestionEntity.createdAt,
+                updatedAt = publicQuestionEntity.updatedAt,
+                createdBy = getNestedUserReferenceToVO(publicQuestionEntity.createdBy),
+                updatedBy = publicQuestionEntity.updatedBy?.let { getNestedUserReferenceToVO(it) },
+                guru = getGuruToVO(publicQuestionEntity.guru)
+            )
+        )
+    }
+
+    private fun getGuruToVO(guruEntity: GuruEntity): GuruVO {
+        return GuruVO(
+            title = guruEntity.title,
+            name = guruEntity.name,
+            specializationId = guruEntity.specializationId,
+            photoUrl = guruEntity.photoUrl,
+            telegramUrl = guruEntity.telegramUrl,
+            youtubeUrl = guruEntity.youtubeUrl
+        )
+    }
+
+    private fun getQuestionSpecializationsToVO(
+        nestedSpecializationEntity: NestedSpecializationEntity
+    ): NestedSpecializationVO {
+        return NestedSpecializationVO(
+            id = nestedSpecializationEntity.id,
+            title = nestedSpecializationEntity.title,
+            description = nestedSpecializationEntity.description,
+            imageSrc = nestedSpecializationEntity.imageSrc,
+            createdAt = nestedSpecializationEntity.createdAt,
+            updatedAt = nestedSpecializationEntity.updatedAt
+        )
+    }
+
+    private fun getQuestionSkillsToVO(nestedSkillEntity: NestedSkillEntity): NestedSkillVO {
+        return NestedSkillVO(
+            id = nestedSkillEntity.id,
+            title = nestedSkillEntity.title,
+            description = nestedSkillEntity.description,
+            imageSrc = nestedSkillEntity.imageSrc,
+            createdAt = nestedSkillEntity.createdAt,
+            updatedAt = nestedSkillEntity.updatedAt
+        )
+    }
+
+    private fun getNestedUserReferenceToVO(
+        nestedUserReferenceEntity: NestedUserReferenceEntity
+    ): NestedUserReferenceVO {
+        return NestedUserReferenceVO(
+            id = nestedUserReferenceEntity.id,
+            username = nestedUserReferenceEntity.username
+        )
     }
 }
--- feature/detail-question/impl/src/main/java/com/example/impl/presentation/viewmodel/DetailQuestionViewModel.kt
+++ feature/detail-question/impl/src/main/java/com/example/impl/presentation/viewmodel/DetailQuestionViewModel.kt
@@ -1,20 +1,22 @@
 package com.example.impl.presentation.viewmodel
 
+import com.example.impl.domain.usecase.GetQuestionByIdUseCase
 import com.example.impl.presentation.intents.DetailQuestionCommand
 import com.example.impl.presentation.intents.DetailQuestionEvent
 import com.example.impl.presentation.mapper.DetailQuestionScreenMapper
-import com.example.impl.presentation.view.DetailQuestionState
+import com.example.impl.presentation.state.DetailQuestionState
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import ru.yeahub.core_utils.BaseViewModel
 
-class DetailQuestionViewModel(detailQuestionScreenMapper: DetailQuestionScreenMapper) : BaseViewModel() {
+class DetailQuestionViewModel(
+    private val detailQuestionScreenMapper: DetailQuestionScreenMapper,
+    private val getQuestionByIdUseCase: GetQuestionByIdUseCase
+) : BaseViewModel() {
 
-    private val _uiState = MutableStateFlow(
-        detailQuestionScreenMapper.getScreenState()
-    )
+    private val _uiState = MutableStateFlow<DetailQuestionState>(DetailQuestionState.Initial)
     val uiState: StateFlow<DetailQuestionState> get() = _uiState
 
     private val _commands = MutableSharedFlow<DetailQuestionCommand>()
@@ -23,6 +25,21 @@ class DetailQuestionViewModel(detailQuestionScreenMapper: DetailQuestionScreenMa
     internal fun handleEvents(event: DetailQuestionEvent) {
         when (event) {
             is DetailQuestionEvent.Todo -> Unit
+            is DetailQuestionEvent.LoadQuestion -> getQuestionById(event.id)
+
+    @Suppress("TooGenericExceptionCaught")
+    private fun getQuestionById(id: Long) {
+        viewModelScopeSafe.launch {
+            _uiState.value = DetailQuestionState.LoadingState
+            try {
+                val question = getQuestionByIdUseCase(id)
+                _uiState.value = detailQuestionScreenMapper.getScreenState(question)
+            } catch (e: Exception) {
+                _uiState.value = DetailQuestionState.ErrorState(e.message)
+            }
+        }
+    }
+
         }
     }
 }
