cf805e33c6183a331cfc26e25be0ef54919aaf86
chisarabiVorts <codeofdevil1999@gmail.com>
2025-07-24 18:34:51 +0300

    ANDROID 50 Создан новый модуль public-questions. Создан макеты экранов ErrorItem, QuestionsItem, LoadingItem(Пустая функция прогресс-Бар), LoadingPlaceholderItem,PublicQuesstionsScreen(Пустая функция для отображения логики экрана)

---
+++ feature/public-questions/impl/src/main/java/ru/yeahub/public_questions/impl/presentation/screen/PublicQuestionsScreen.kt
@@ -0,0 +1,10 @@
+package ru.yeahub.public_questions.impl.presentation.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PublicQuestionsScreen() {
+    /**
+     * Логика отображения экрана
+     */
+}
