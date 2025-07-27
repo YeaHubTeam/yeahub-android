2bb45cfaa38d50044b5387a0fe8f214fdf53d839
chisarabiVorts <codeofdevil1999@gmail.com>
2025-07-27 17:03:59 +0300

    ANDROID-50: Верстка списка вопросов БЕЗ ФИЛЬТРОВ (#41)
    * ANDROID 50 Создан новый модуль public-questions. Создан макеты экранов ErrorItem, QuestionsItem, LoadingItem(Пустая функция прогресс-Бар), LoadingPlaceholderItem,PublicQuesstionsScreen(Пустая функция для отображения логики экрана)
    
    * ANDROID 50 Исправление ktlinc
    
    * ANDROID 50 Исправление ktlinc 2.0
    
    * ANDROID 50 Удалены не нужные файлы.

---
+++ feature/public-questions/api/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "ru.yeahub.public_questions.api"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:navigation-api"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.android)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ feature/public-questions/impl/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "ru.yeahub.public_questions.impl"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt")
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = false
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:navigation-api"))
+    implementation(project(":core:network-api"))
+    implementation(project(":feature:public-questions:api"))
+    implementation(project(":core:utils"))
+    implementation(project(":core:ui"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.android)
+
+    implementation(libs.compose.shimmer)
+
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
+    // Navigation dependencies
+    implementation(libs.androidx.navigation.compose)
+
+    // Timber
+    implementation(libs.timber)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
