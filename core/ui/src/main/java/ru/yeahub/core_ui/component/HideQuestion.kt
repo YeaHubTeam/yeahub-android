502ac65de437206acb8d2af8717c1e3690bac726
funnyminorudev <funnyminoru@gmail.com>
2025-08-04 11:20:16 +0300

    ANDROID 54: Изменения компонентов под требования

--- core/ui/src/main/java/ru/yeahub/core_ui/component/HideQuestion.kt
+++ core/ui/src/main/java/ru/yeahub/core_ui/component/HideQuestion.kt
@@ -2,8 +2,6 @@ package ru.yeahub.core_ui.component
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.EaseInOut
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
@@ -17,6 +15,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -31,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -56,12 +54,6 @@ fun HideQuestion(
     onToggle: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val rotation by animateFloatAsState(
-        targetValue = if (!isExtended) 0f else -180f,
-        animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing),
-        label = "rotate",
-    )
-
     Surface(
         modifier = modifier
             .padding(10.dp)
@@ -94,24 +86,11 @@ fun HideQuestion(
 
                 Text(
                     text = questionText,
-                    modifier = Modifier.weight(1f),
+                    modifier = Modifier.fillMaxWidth().height(21.dp),
                     style = LocalAppTypography.current.body3Accent.copy(lineHeight = 20.sp)
                 )
 
                 Spacer(modifier = Modifier.width(8.dp))
-
-                Box(
-                    modifier = Modifier.size(24.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.arrow_vector),
-                        contentDescription = null,
-                        modifier = Modifier.graphicsLayer {
-                            rotationZ = rotation
-                        }
-                    )
-                }
             }
 
             AnimatedContent(
--- feature/public-questions/impl/src/main/java/ru/yeahub/public_questions/impl/presentation/views/PublicQuestionsItem.kt
+++ feature/public-questions/impl/src/main/java/ru/yeahub/public_questions/impl/presentation/views/PublicQuestionsItem.kt
@@ -1,8 +1,18 @@
 package ru.yeahub.public_questions.impl.presentation.views
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import ru.yeahub.core_ui.component.HideQuestion
+import ru.yeahub.core_ui.example.dynamicPreview.StandardScreenSizePreview
+import ru.yeahub.core_ui.example.staticPreview.StaticPreview
 import ru.yeahub.public_questions.impl.presentation.screen.PublicQuestionUiModel
 
 @Composable
@@ -22,65 +32,47 @@ fun PublicQuestionsItem(
         modifier = Modifier
     )
 }
+
 /**
  * Static preview
-
+ */
 @StaticPreview
 @Composable
 fun QuestionsItemPreview() {
-val mockQuestions = listOf(
-PublicQuestionUiModel(
-id = 1,
-title = "Что такое Virtual DOM?",
-),
-PublicQuestionUiModel(
-id = 2,
-title = "Что такое Redux?",
-),
-PublicQuestionUiModel(
-id = 3,
-title = "Что такое Kotlin?",
-),
-PublicQuestionUiModel(
-id = 4,
-title = "Что такое TypeScript?",
-),
-PublicQuestionUiModel(
-id = 5,
-title = "Что такое функциональное программирование?",
-),
-PublicQuestionUiModel(
-id = 6,
-title = "Что такое Monads в функциональном программировании?",
-),
-PublicQuestionUiModel(
-id = 7,
-title = "Что такое Java?",
-)
-)
-
-/* QuestionsItem(
-questions = mockQuestions,
-onClickMore = { itemId, title -> },
-
-)*/
+    val mockQuestions = listOf(
+        PublicQuestionUiModel(id = 1, title = "Что такое Virtual DOM?"),
+        PublicQuestionUiModel(id = 2, title = "Что такое Redux?"),
+        PublicQuestionUiModel(id = 3, title = "Что такое Kotlin?"),
+        PublicQuestionUiModel(id = 4, title = "Что такое TypeScript?"),
+        PublicQuestionUiModel(id = 5, title = "Что такое функциональное программирование?"),
+        PublicQuestionUiModel(id = 6, title = "Что такое Monads в функциональном программировании?"),
+        PublicQuestionUiModel(id = 7, title = "Что такое Java?")
+    )
+    LazyColumn {
+        items(mockQuestions) { question ->
+            PublicQuestionsItem(
+                questions = question,
+                onClickMore = { itemId -> }
+            )
+        }
+    }
 }
 
 /**
  * Dinamic preview
-*/
+ */
 class MockQuestionsViewModel : ViewModel() {
-private val _state = MutableStateFlow<List<PublicQuestionUiModel>>(emptyList())
-val state: Flow<List<PublicQuestionUiModel>> get() = _state.asStateFlow()
+    private val _state = MutableStateFlow<List<PublicQuestionUiModel>>(emptyList())
+    val state: Flow<List<PublicQuestionUiModel>> get() = _state.asStateFlow()
 
-init {
-_state.value = List(TEST) { index ->
-PublicQuestionUiModel(
-id = index.toLong(),
-title = "Mocked Question Title $index",
-)
-}
-}
+    init {
+        _state.value = List(TEST) { index ->
+            PublicQuestionUiModel(
+                id = index.toLong(),
+                title = "Mocked Question Title $index",
+            )
+        }
+    }
 }
 
 private const val TEST = 10
@@ -88,11 +80,15 @@ private const val TEST = 10
 @StandardScreenSizePreview
 @Composable
 fun PreviewQuestionsScreen() {
-val viewModel: MockQuestionsViewModel = viewModel()
-val state = viewModel.state.collectAsState(emptyList())
+    val viewModel: MockQuestionsViewModel = viewModel()
+    val state = viewModel.state.collectAsState(emptyList())
 
-QuestionsItem(
-questions = state.value,
-onClickMore = { id, title -> },
-)
-}**/
+    LazyColumn {
+        items(state.value) { question ->
+            PublicQuestionsItem(
+                questions = question,
+                onClickMore = { id -> }
+            )
+        }
+    }
+}
