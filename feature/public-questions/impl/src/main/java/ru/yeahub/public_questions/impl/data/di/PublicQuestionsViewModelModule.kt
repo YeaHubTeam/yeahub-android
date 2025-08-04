ee6c1be631963d37d3c08f78ed6ce78b330d7ad3
funnyminorudev <funnyminoru@gmail.com>
2025-08-04 10:50:03 +0300

    ANDROID 54: Добавлен PublicQuestionsViewModel и модуль для Di

---
+++ feature/public-questions/impl/src/main/java/ru/yeahub/public_questions/impl/data/di/PublicQuestionsViewModelModule.kt
@@ -0,0 +1,16 @@
+package ru.yeahub.public_questions.impl.data.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.yeahub.public_questions.impl.presentation.viewmodel.PublicQuestionsViewModel
+
+val questionsViewModelModule = module {
+    viewModel { (skills: List<String>?, skillFilter: String?) ->
+        PublicQuestionsViewModel(
+            getPublicQuestionsUseCase = get(),
+            mapper1 = get(),
+            skills = skills,
+            skillFilter = skillFilter
+        )
+    }
+}
