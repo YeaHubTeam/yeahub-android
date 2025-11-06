9d3a50d04ee5ee53b79c3a28059db715fd53cc1f
Daniil-Tomaylyi <daniiltomajly@gmail.com>
2025-11-06 14:23:12 +0300

    ANDR-11 исправил баг с невозможностью скрола на экранах главного меню, коллекций и вопросов

--- feature/example-home/impl/src/main/java/ru/yeahub/example_home/impl/presentation/view/QuestionsMainScreen.kt
+++ feature/example-home/impl/src/main/java/ru/yeahub/example_home/impl/presentation/view/QuestionsMainScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -61,7 +63,7 @@ fun QuestionsMainScreenContent(
     onBackClick: () -> Unit
 ) {
     val context = LocalContext.current
-
+    val scrollState = rememberScrollState()
     when (state) {
         is QuestionMainScreenState.Loading -> {
             Box(
@@ -89,6 +91,7 @@ fun QuestionsMainScreenContent(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(top = 24.dp)
+                    .verticalScroll(scrollState)
             ) {
                 //Заголовок
                 Text(
