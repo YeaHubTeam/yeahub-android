f3f6b8c440c561d22a998f2019bd66a02442adf8
PanMobile <chronosroseviknr@gmail.com>
2026-01-08 21:02:48 +0300

    Добавление необходимых ресурсов для верстки первого экрана экрана CreateQuiz

---
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/presentation/VoSpecialization.kt
@@ -0,0 +1,9 @@
+package ru.yeahub.interview_trainer.impl.createQuiz.presentation
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class VoSpecialization(
+    val id: Int,
+    val title: String
+)
