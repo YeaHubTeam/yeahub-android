a51b0cfb77c2f3a7812006226d0e18d349a14063
Artem_Mih <149761873+PanMobile@users.noreply.github.com>
2026-01-26 00:19:46 +0300

    ANDR-5: Инициализация фичи Interview Trainer и верстка первого экрана CreateQuiz (#94)
    * Создан модуль Interview_Trainer
    
    * Моделирование состояний первого экрана интервью тренажера - CreateQuiz
    
    * Добавление необходимых ресурсов для верстки первого экрана экрана CreateQuiz
    
    * Добавление базовых классов фичи
    
    * Базовая верстка первого экрана CreateQuizScreen.kt
    
    * Добавление объекта интервью тренажера в пути фич для будущей навигации
    
    * ANDR-5: VoSpecialization перенесен внутри Loaded
    
    * ANDR-5: SkillButton.kt компонент изменен. Убрана полная заливка кнопки при активации/нажатии на нее
    
    * ANDR-5: Добавлены дополнительные строковые ресурсы для возможных ошибок
    
    * ANDR-5: Создание превью и правки по отступам
    
    * ANDR-5: Экран загрузки CreateQuiz
    
    * ANDR-5: Использование экрана загрузки в главном для @StaticPreview
    
    * ANDR-5: Удалены неиспользуемые строки ошибок
    
    * ANDR-5: Убран лишний класс параметров. В StaticPreview передаем сразу стейт
    
    * ANDR-5: Убрана преписка Mock у @Composable
    
    * ANDR-5: id стало Long по аналогии с другими классами
    
    * ANDR-5: Command'ы и Result'ы убраны из папки intents. Сама папка удалена
    
    * ANDR-5: Создан интерфейс юзкейса
    
    * ANDR-5: Созданы Domain модели
    
    * ANDR-5: ВьюМоделька экрана CreateQuiz. Сделана не до конца
    
    * ANDR-5: Изменение кол-ва вопросов через данные у ивента
    
    * ANDR-5: изменил путь файла
    
    * ANDR-5: Изменение кода экрана. Настроено правильное прокидывание лямбд в UI
    
    * ANDR-5: Сделано Динамическое превью
    
    * ANDR-5: убраны default параметры
    
    * ANDR-5: убраны лишние классы
    
    * ANDR-5: Создан маппер
    
    * ANDR-5: имплементирован маппер и отдельный флоу ввода пользователя для изменения стейта
    
    * ANDR-5: сделано рабочее динамик превью
    
    * ANDR-5: убран default диспатчер при обновлении ввода пользователя
    
    * ANDR-5: убраны параметры по умолчанию в верстке

---
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/presentation/CreateQuizEvent.kt
@@ -0,0 +1,16 @@
+package ru.yeahub.interview_trainer.impl.createQuiz.presentation
+
+sealed interface CreateQuizEvent {
+    data class OnSpecializationClick(val specializationId: Long) : CreateQuizEvent
+
+    data class OnPlusQuestionClick(val questionsCount: Int) : CreateQuizEvent
+
+    data class OnMinusQuestionClick(val questionsCount: Int) : CreateQuizEvent
+
+    data class OnStartInterviewQuizClick(
+        val specializationId: Long,
+        val questionCount: Int
+    ) : CreateQuizEvent
+
+    data object OnBackClick : CreateQuizEvent
+}
---
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/ui/CreateQuizScreenLoading.kt
@@ -0,0 +1,131 @@
+package ru.yeahub.interview_trainer.impl.createQuiz.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
+import ru.yeahub.core_ui.component.SecondaryButton
+import ru.yeahub.core_ui.example.staticPreview.StaticPreview
+import ru.yeahub.core_ui.theme.LocalAppTypography
+import ru.yeahub.core_ui.theme.colors
+import ru.yeahub.interview_trainer.impl.R
+
+private val FIGMA_HORIZONTAL_PADDING = 16.dp
+private val FIGMA_VERTICAL_BLOCKS_PADDING = 16.dp
+private val FIGMA_VERTICAL_FIRST_AND_LAST_ELEMENT_PADDING = 24.dp
+
+@StaticPreview
+@Composable
+fun CreateQuizLoading(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = FIGMA_HORIZONTAL_PADDING)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(vertical = 24.dp),
+            style = LocalAppTypography.current.head5,
+            text = stringResource(R.string.create_quiz_screen_main_title),
+            color = colors.black900
+        )
+
+        PlaceHolderBlock()
+
+        Spacer(modifier = Modifier.height(FIGMA_VERTICAL_BLOCKS_PADDING))
+
+        PlaceHolderBlock()
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        DisabledStartQuizButton()
+    }
+}
+
+@Composable
+private fun PlaceHolderBlock(
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .shimmer(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .background(Color.LightGray, shape = RoundedCornerShape(4.dp))
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(148.dp)
+                .background(Color.LightGray, shape = RoundedCornerShape(4.dp))
+        )
+    }
+}
+
+@Composable
+private fun DisabledStartQuizButton(
+    modifier: Modifier = Modifier,
+) {
+    SecondaryButton(
+        modifier = modifier
+            .padding(vertical = FIGMA_VERTICAL_FIRST_AND_LAST_ELEMENT_PADDING)
+            .height(48.dp)
+            .fillMaxWidth(),
+        enabled = false,
+        onClick = { }
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.create_quiz_start_quiz_button_text),
+                style = LocalAppTypography.current.body3Strong,
+                textAlign = TextAlign.Center,
+                color = colors.white900
+            )
+
+            Spacer(Modifier.width(8.dp))
+
+            Icon(
+                modifier = Modifier.size(16.dp),
+                painter = painterResource(ru.yeahub.ui.R.drawable.arrow_next),
+                contentDescription = "Start Interview Quiz Session",
+                tint = colors.white900
+            )
+        }
+    }
+}
