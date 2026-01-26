9c7b68fb4f2c1cbf7efb7abd11528df8a9be0b0c
PanMobile <chronosroseviknr@gmail.com>
2026-01-26 17:45:43 +0300

    ANDR-5: Создан маппер дата в домейн

---
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/data/CreateQuizDataToDomainMapper.kt
@@ -0,0 +1,25 @@
+package ru.yeahub.interview_trainer.impl.createQuiz.data
+
+import ru.yeahub.interview_trainer.impl.createQuiz.domain.DomainSpecialization
+import ru.yeahub.interview_trainer.impl.createQuiz.domain.DomainSpecializationListResponse
+import ru.yeahub.network_api.models.GetSpecializationResponse
+import ru.yeahub.network_api.models.GetSpecializationsResponse
+
+class CreateQuizDataToDomainMapper {
+    fun dataListToDomainList(
+        dataResponse: GetSpecializationsResponse,
+    ): DomainSpecializationListResponse =
+        DomainSpecializationListResponse(
+            page = dataResponse.page ?: 1L,
+            limit = dataResponse.limit ?: 10L,
+            data = dataResponse.data.map { dataItem -> dataToDomain(dataItem) },
+            total = dataResponse.total
+        )
+
+    //функция публичная для тестов
+    fun dataToDomain(data: GetSpecializationResponse): DomainSpecialization =
+        DomainSpecialization(
+            id = data.id,
+            title = data.title
+        )
+}
