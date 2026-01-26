a10e249e71fe226250fc5e039f158ac7da0f3437
PanMobile <chronosroseviknr@gmail.com>
2026-01-26 17:53:14 +0300

    ANDR-5: Юзкейс убран (на данном этапе не нужен)

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/domain/GetSpecializationsListUseCase.kt
+++
@@ -1,9 +0,0 @@
-package ru.yeahub.interview_trainer.impl.createQuiz.domain
-
-class GetSpecializationsListUseCase(
-    private val repository: CreateQuizRepositoryApi,
-) {
-    suspend operator fun invoke(
-        request: SpecializationsRequest,
-    ): DomainSpecializationListResponse = repository.getSpecializationsList(request = request)
-}
