c1f2779e20f0baf2fb87e7e18fabbd6114a0ad64
Yrun00 <danil.budarenkov@gmail.com>
2026-02-01 14:28:57 +0400

    added UnsavedChangesDialog with preview

---
+++ core/ui/src/main/java/ru/yeahub/core_ui/component/AlertDialog.kt
@@ -0,0 +1,44 @@
+package ru.yeahub.core_ui.component
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import ru.yeahub.core_ui.example.dynamicPreview.SmallScreenSizePreview
+
+@Composable
+fun UnsavedChangesDialog(
+    onStay: () -> Unit,
+    onLeave: () -> Unit,
+) {
+    AlertDialog(
+        onDismissRequest = onStay,
+        title = { Text("Подтвердить действие") },
+        text = {
+            Text(
+                "У вас есть несохраненные данные.\nВы хотите продолжить?",
+            )
+        },
+        dismissButton = {
+            PrimaryButton(onClick = onStay) {
+                Text("Да")
+            }
+        },
+        confirmButton = {
+            OutlineButton(
+                onClick = onLeave,
+                colors = YeahubButtonDefaults.secondaryOutlinedButtonColors(),
+                border = YeahubButtonDefaults.secondaryOutlineBorderDefaults(),
+            ) {
+                Text(
+                    "нет",
+                )
+            }
+        },
+    )
+}
+
+@SmallScreenSizePreview
+@Composable
+fun UnsavedChangesDialogPreview() {
+    UnsavedChangesDialog({}, {})
+}
