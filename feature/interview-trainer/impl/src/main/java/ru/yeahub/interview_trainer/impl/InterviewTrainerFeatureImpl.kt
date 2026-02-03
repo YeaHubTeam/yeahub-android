36b62216b5d985382b64b340341febd692880455
PanMobile <chronosroseviknr@gmail.com>
2026-02-03 22:34:08 +0300

    ANDR-71: Добавлена реализация InterviewTrainerFeatureImpl. Внутри регистрация графа, обработка навигации назад и обработка навигации к экрану тренировки

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/InterviewTrainerFeatureImpl.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/InterviewTrainerFeatureImpl.kt
@@ -3,11 +3,14 @@ package ru.yeahub.interview_trainer.impl
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import ru.yeahub.interview_trainer.api.InterviewTrainerApi
 import ru.yeahub.navigation_api.FeatureApi
 import ru.yeahub.navigation_api.FeatureRoute
 import ru.yeahub.navigation_api.NavigationPathManager
+import timber.log.Timber
 
-class InterviewTrainerFeatureImpl : FeatureApi {
+class InterviewTrainerFeatureImpl(private val trainerApi: InterviewTrainerApi) : FeatureApi {
     override fun getFeatureName(): String = FeatureRoute.InterviewTrainerFeature.FEATURE_NAME
 
     override fun registerGraph(
@@ -16,6 +19,79 @@ class InterviewTrainerFeatureImpl : FeatureApi {
         pathManager: NavigationPathManager,
         modifier: Modifier,
     ) {
-        TODO("Not yet implemented")
+        val currentPath = pathManager.getCurrentPath()
+        Timber.d("InterviewTrainerFeatureImpl registerGraph: currentPath: $currentPath")
+
+        val createQuizRoute = if (currentPath.isEmpty()) {
+            getFeatureName()
+        } else {
+            pathManager.createChildPath(getFeatureName())
+        }
+        Timber.d("InterviewTrainerFeatureImpl registerGraph: Registering route: $createQuizRoute")
+
+        navGraphBuilder.composable(createQuizRoute) {
+            trainerApi.CreateQuizScreen(
+                onBackClick = { handleBackNavigation(pathManager, navController) },
+
+                onStartTrainingClick = { specializationId, questionsCount ->
+                    handleQuizNavigation(
+                        pathManager,
+                        navController,
+                        specializationId,
+                        questionsCount
+                    )
+                }
+            )
+        }
+    }
+
+    /**
+     * Обработка навигации назад.
+     */
+    private fun handleBackNavigation(
+        pathManager: NavigationPathManager,
+        navController: NavHostController,
+    ) {
+        val parentPath = pathManager.getCurrentPath()
+        Timber.d("InterviewTrainerFeatureImpl handleBackNavigation: Navigating to parent: $parentPath")
+
+        pathManager.setCurrentPath(parentPath)
+
+        if (parentPath.isEmpty()) {
+            navController.navigateUp()
+        } else {
+            navController.navigate(parentPath) {
+                popUpTo(parentPath) {
+                    inclusive = true
+                }
+            }
+        }
+    }
+
+    /**
+     * Обработка навигации к экрану тренировки (InterviewQuizScreen).
+     */
+    private fun handleQuizNavigation(
+        pathManager: NavigationPathManager,
+        navController: NavHostController,
+        specializationId: String,
+        questionsCount: String,
+    ) {
+        pathManager.setCurrentPath(getFeatureName())
+
+        // Используем текущий путь как базу для экрана тренировки
+        val quizPath = pathManager.createParametrizedPath(
+            featureName = "quiz",
+            "specializationId", "questionCount"
+        )
+
+        val concretePath = pathManager.createConcretePath(
+            parametrizedPath = quizPath,
+            specializationId, questionsCount
+        )
+        Timber.d("InterviewTrainerFeatureImpl handleQuizNavigation: Navigating to: $concretePath")
+
+        pathManager.setCurrentPath(concretePath)
+        navController.navigate(concretePath)
     }
 }
