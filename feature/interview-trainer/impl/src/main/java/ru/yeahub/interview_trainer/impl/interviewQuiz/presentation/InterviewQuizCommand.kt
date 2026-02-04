77c65680e50c2676adc622e7f8eb14b1cb6e3763
Deyryl <snegurivan.of@mail.ru>
2026-02-04 16:49:51 +0300

    ANDR-58: из Command и Event наследники заменены на ToDo

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuiz/presentation/InterviewQuizCommand.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuiz/presentation/InterviewQuizCommand.kt
@@ -2,7 +2,5 @@ package ru.yeahub.interview_trainer.impl.interviewQuiz.presentation
 
 sealed interface InterviewQuizCommand {
 
-    data object NavigateToInterviewQuizResultScreen : InterviewQuizCommand
-
-    data object NavigateBack : InterviewQuizCommand
+    data object ToDo : InterviewQuizCommand
 }
