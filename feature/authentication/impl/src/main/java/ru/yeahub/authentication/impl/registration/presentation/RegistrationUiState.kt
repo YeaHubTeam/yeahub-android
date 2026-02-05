027dd92b65e354f80bc0bfbcb88e0716eac0f204
MODDII <moddiixs@gmail.com>
2026-02-05 19:17:12 +0300

    Исправлены замечания ktlint и detekt

--- feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/RegistrationUiState.kt
+++ feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/RegistrationUiState.kt
@@ -5,29 +5,23 @@ data class RegistrationUiState(
     val email: String,
     val password: String,
     val confirmPassword: String,
-
     val isPdAccepted: Boolean,
     val isOfferAccepted: Boolean,
     val isMailingAccepted: Boolean,
-
     val isPasswordVisible: Boolean,
     val isConfirmPasswordVisible: Boolean,
-
     val isSubmitEnabled: Boolean
 )
 
 sealed class RegistrationAction {
-    data class NicknameChanged(val value: String): RegistrationAction()
-    data class EmailChanged(val value: String): RegistrationAction()
-    data class PasswordChanged(val value: String): RegistrationAction()
-    data class ConfirmPasswordChanged(val value: String): RegistrationAction()
-
-    data class PdAcceptedChanged(val value: Boolean): RegistrationAction()
-    data class OfferAcceptedChanged(val value: Boolean): RegistrationAction()
-    data class MailingAcceptedChanged(val value: Boolean): RegistrationAction()
-
-    data object TogglePasswordVisible: RegistrationAction()
-    data object ToggleConfirmPasswordVisible: RegistrationAction()
-
-    data object SubmitClicked: RegistrationAction()
+    data class NicknameChanged(val value: String) : RegistrationAction()
+    data class EmailChanged(val value: String) : RegistrationAction()
+    data class PasswordChanged(val value: String) : RegistrationAction()
+    data class ConfirmPasswordChanged(val value: String) : RegistrationAction()
+    data class PdAcceptedChanged(val value: Boolean) : RegistrationAction()
+    data class OfferAcceptedChanged(val value: Boolean) : RegistrationAction()
+    data class MailingAcceptedChanged(val value: Boolean) : RegistrationAction()
+    data object TogglePasswordVisible : RegistrationAction()
+    data object ToggleConfirmPasswordVisible : RegistrationAction()
+    data object SubmitClicked : RegistrationAction()
 }
