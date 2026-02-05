ff7582ddc06c46a51c152cf2fe8e2a3cab39d77b
PanMobile <chronosroseviknr@gmail.com>
2026-02-05 23:10:35 +0300

    ANDR-71: Исправлен DI модуль для юзкейса

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/di/CreateQuizUseCaseModule.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/createQuiz/di/CreateQuizUseCaseModule.kt
@@ -2,7 +2,12 @@ package ru.yeahub.interview_trainer.impl.createQuiz.di
 
 import org.koin.dsl.module
 import ru.yeahub.interview_trainer.impl.createQuiz.domain.GetSpecializationsListUseCaseImpl
+import ru.yeahub.interview_trainer.impl.createQuiz.domain.GetSpecializationsUseCase
 
 val createQuizUseCaseModule = module {
-    factory { GetSpecializationsListUseCaseImpl(repository = get()) }
+    factory<GetSpecializationsUseCase> {
+        GetSpecializationsListUseCaseImpl(
+            repository = get()
+        )
+    }
 }
