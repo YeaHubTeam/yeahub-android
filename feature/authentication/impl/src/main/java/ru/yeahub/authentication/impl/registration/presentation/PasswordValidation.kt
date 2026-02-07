da9ba364531e7ce31443680c9efd1d76e464d937
MODDII <moddiixs@gmail.com>
2026-02-07 15:10:18 +0300

    Исправил валидацию пароля (regex), вынес строки в ресурсы, почистил зависимости API-модуля

--- feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/PasswordValidation.kt
+++ feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/PasswordValidation.kt
@@ -3,9 +3,8 @@ package ru.yeahub.authentication.impl.registration.presentation
 private const val MIN_PASSWORD_LENGTH = 8
 
 internal fun isPasswordValid(password: String): Boolean {
-    if (password.length < MIN_PASSWORD_LENGTH) return false
-    val hasDigit = password.any(Char::isDigit)
-    val hasLower = password.any { it in 'a'..'z' }
-    val hasUpper = password.any { it in 'A'..'Z' }
-    return hasDigit && hasLower && hasUpper
+    val isValid = password.matches(
+        Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{$MIN_PASSWORD_LENGTH,}$")
+    )
+    return isValid
 }
