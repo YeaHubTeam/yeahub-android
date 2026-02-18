7631fdc9cb7604a717f8b980294875315b42e1e1
Yrun00 <danil.budarenkov@gmail.com>
2026-02-18 23:09:35 +0400

    state small refactor

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/ProfileEditState.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/ProfileEditState.kt
@@ -9,18 +9,10 @@ sealed interface ProfileEditState {
     @Immutable
     data class Loaded(
         val selectedTab: ProfileEditTabs,
-        val avatarUrl: String?,
-        val nickname: String,
-        val specializationList: List<String>,
-        val specialization: String,
-        val email: String,
-        val location: String,
-        val socialLinksUrlMap: Map<SocialLinks, String>,
-    ) : ProfileEditState {
-        enum class SocialLinks {
-            Instagram, Linkedin, Twitter, GitHub, Behance, Whatsapp, Telegram, VK, Dribble,
-        }
-    }
+        val personalInfoState: PersonalInfoTabState,
+        val aboutMeTabState: AboutMeTabState,
+        val skillsTabState: SkillsTabState,
+    ) : ProfileEditState
 
     data class Error(val throwable: Throwable) : ProfileEditState
 
@@ -28,4 +20,32 @@ sealed interface ProfileEditState {
         val index: Int,
         val title: String,
     ) { PersonalInfo(0, "Личная информация"), AboutMe(1, "Обо мне"), Skills(2, "Навыки") }
+
+    enum class SocialLinks {
+        Instagram, Linkedin, Twitter, GitHub, Behance, Whatsapp, Telegram, VK, Dribble,
+    }
+
+    data class PersonalInfoTabState(
+        val avatarUrl: String?,
+        val nickname: String,
+        val specializationList: List<String>,
+        val specialization: String,
+        val email: String,
+        val location: String,
+        val socialLinksUrlMap: Map<SocialLinks, String>,
+    )
+
+    data class AboutMeTabState(val aboutMeField: String)
+
+    data class SkillsTabState(
+        val listOfSkills: List<Skill>,
+        val listOfChosenSkills: List<Skill>,
+    )
+
+    data class Skill(
+        val image: Int,
+        val name: String,
+    )
 }
+
+
