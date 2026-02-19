f63910dd3c417b5a842152e0baa08d4637584569
PanMobile <chronosroseviknr@gmail.com>
2026-02-19 12:32:12 +0300

    ANDR-71: исправлены тесты

--- feature/interview-trainer/impl/src/test/java/test/CreateQuizScreenMapperTest.kt
+++ feature/interview-trainer/impl/src/test/java/test/CreateQuizScreenMapperTest.kt
@@ -1,5 +1,6 @@
 package test
 
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
@@ -50,13 +51,13 @@ class CreateQuizScreenMapperTest {
         )
 
         val defaultLoadedState = CreateQuizState.Loaded(
-            specializations = listOf(defaultVoSpecial, defaultVoSpecialWithImage),
+            specializations = persistentListOf(defaultVoSpecial, defaultVoSpecialWithImage),
             selectedSpecializationId = 0L,
             questionsCount = 10
         )
 
         val loadedStateWithDifferentSelection = CreateQuizState.Loaded(
-            specializations = listOf(defaultVoSpecial, defaultVoSpecialWithImage),
+            specializations = persistentListOf(defaultVoSpecial, defaultVoSpecialWithImage),
             selectedSpecializationId = 1L,
             questionsCount = 5
         )
