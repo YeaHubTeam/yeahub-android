0520b9672f8c426e66d4ebc09e7cf44b66bf192d
Yrun00 <danil.budarenkov@gmail.com>
2026-03-17 22:08:28 +0400

    ProfileEditScreenEvent

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenEvent.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenEvent.kt
@@ -1,5 +1,30 @@
 package ru.yeahub.profile_edit.impl.presentation.intents
 
+import android.net.Uri
+import ru.yeahub.profile_edit.impl.presentation.ProfileEditState
+
 sealed interface ProfileEditScreenEvent {
-    data object ToDo : ProfileEditScreenEvent
+
+    data object LoadData : ProfileEditScreenEvent
+    data class SwitchTab(val tab: ProfileEditState.ProfileEditTabs) : ProfileEditScreenEvent
+    data object BackPressed : ProfileEditScreenEvent
+    data object DiscardChanges : ProfileEditScreenEvent
+    data object SaveProfile : ProfileEditScreenEvent
+
+    data object UploadAvatar : ProfileEditScreenEvent
+    data class OnAvatarSelected(val uri: Uri) : ProfileEditScreenEvent
+    data object DeleteAvatar : ProfileEditScreenEvent
+    data class OnNicknameChanged(val nickname: String) : ProfileEditScreenEvent
+    data class ChooseSpecialization(val specialization: String) : ProfileEditScreenEvent
+    data object CannotChangeSpecializationToast : ProfileEditScreenEvent
+    data class OnLocationChanged(val location: String) : ProfileEditScreenEvent
+    data class OnSocialLinkChanged(
+        val link: ProfileEditState.SocialLinks,
+        val url: String,
+    ) : ProfileEditScreenEvent
+
+    data class OnAboutMeChanged(val text: String) : ProfileEditScreenEvent
+
+    data class AddSkill(val skill: ProfileEditState.Skill) : ProfileEditScreenEvent
+    data class RemoveSkill(val skill: ProfileEditState.Skill) : ProfileEditScreenEvent
 }
