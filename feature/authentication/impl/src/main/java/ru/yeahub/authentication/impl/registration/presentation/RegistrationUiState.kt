fcc4809aef3e3efaff4b0e1f5b678649242e7a59
xMODDIIx <91845532+xMODDIIx@users.noreply.github.com>
2026-03-18 20:33:24 +0300

    Feature/andr 50 Логика презентационного слоя + DI (#121)
    * Добавил registrationUseCase в RegistrationViewModel.Так же добавил два стейта в RegistrationUiState
    
    * Настроил Koin DI для модуля регистрации
    
    * Fix build
    
    * Fix 2
    
    * Рефакторинг презентационного слоя: переход на MVI и выделение маппера
    
    * удаление дефолтных значений и централизация стейта в маппере

--- feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/RegistrationUiState.kt
+++ feature/authentication/impl/src/main/java/ru/yeahub/authentication/impl/registration/presentation/RegistrationUiState.kt
@@ -1,6 +1,6 @@
 package ru.yeahub.authentication.impl.registration.presentation
 
-data class RegistrationUiState(
+data class RegistrationFormState(
     val nickname: String,
     val email: String,
     val password: String,
@@ -10,18 +10,30 @@ data class RegistrationUiState(
     val isMailingAccepted: Boolean,
     val isPasswordVisible: Boolean,
     val isConfirmPasswordVisible: Boolean,
-    val isSubmitEnabled: Boolean
+    val isSubmitEnabled: Boolean,
 )
 
-sealed class RegistrationAction {
-    data class NicknameChanged(val value: String) : RegistrationAction()
-    data class EmailChanged(val value: String) : RegistrationAction()
-    data class PasswordChanged(val value: String) : RegistrationAction()
-    data class ConfirmPasswordChanged(val value: String) : RegistrationAction()
-    data class PdAcceptedChanged(val value: Boolean) : RegistrationAction()
-    data class OfferAcceptedChanged(val value: Boolean) : RegistrationAction()
-    data class MailingAcceptedChanged(val value: Boolean) : RegistrationAction()
-    data object TogglePasswordVisible : RegistrationAction()
-    data object ToggleConfirmPasswordVisible : RegistrationAction()
-    data object SubmitClicked : RegistrationAction()
+sealed interface RegistrationUiState {
+    val formState: RegistrationFormState
+
+    data class Content(override val formState: RegistrationFormState) :
+        RegistrationUiState
+
+    data class Loading(override val formState: RegistrationFormState) : RegistrationUiState
+
+    data class Error(val message: String, override val formState: RegistrationFormState) :
+        RegistrationUiState
+}
+
+sealed interface RegistrationAction {
+    data class NicknameChanged(val value: String) : RegistrationAction
+    data class EmailChanged(val value: String) : RegistrationAction
+    data class PasswordChanged(val value: String) : RegistrationAction
+    data class ConfirmPasswordChanged(val value: String) : RegistrationAction
+    data class PdAcceptedChanged(val value: Boolean) : RegistrationAction
+    data class OfferAcceptedChanged(val value: Boolean) : RegistrationAction
+    data class MailingAcceptedChanged(val value: Boolean) : RegistrationAction
+    data object TogglePasswordVisible : RegistrationAction
+    data object ToggleConfirmPasswordVisible : RegistrationAction
+    data object SubmitClicked : RegistrationAction
 }
