9d59ee2678ac7d8b91db1e4056e8a9e40aac915b
Alex Bauman <103599028+alex-bauman-88@users.noreply.github.com>
2026-03-20 17:25:00 +0100

    ANDR-45 Сделана верстка подэкрана "навыки" (Верстка редактирования профиля) и небольшие правки подэкрана "обо мне".

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/ui/tabs/ProfileEditAboutMeTab.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/ui/tabs/ProfileEditAboutMeTab.kt
@@ -1,21 +1,18 @@
 package ru.yeahub.profile_edit.impl.ui.tabs
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ru.yeahub.core_ui.component.textInput.TextInputColorsDefaults
+import ru.yeahub.core_ui.component.textInput.DefaultTextField
 import ru.yeahub.core_ui.theme.Theme
 import ru.yeahub.profile_edit.impl.presentation.ProfileEditState
 import ru.yeahub.profile_edit.impl.ui.SectionTitle
@@ -30,60 +27,36 @@ private val PURPLE_AREA_TEXT_SPACER = 8.dp
 fun AboutMeContent(
     state: ProfileEditState.AboutMeTabState
 ) {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(Theme.colors.white900)
             .padding(top = ABOUT_ME_TOP_PADDING),
     ) {
-        item {
-            Spacer(Modifier.padding(ABOUT_ME_TOP_SPACER))
-            SectionTitle(title = stringResource(R.string.about_me_tab))
+        Spacer(Modifier.padding(ABOUT_ME_TOP_SPACER))
+        SectionTitle(title = stringResource(R.string.about_me_tab))
 
-            Spacer(Modifier.padding(ABOUT_ME_MIDDLE_SPACER))
-            Text(
-                text = stringResource(R.string.about_me_tab_label),
-                style = Theme.typography.body7Alt,
-                color = Theme.colors.black900,
-                modifier = Modifier.fillMaxWidth(),
-            )
-        }
-        item {
-            Spacer(Modifier.padding(PURPLE_AREA_TEXT_SPACER))
-            PurpleTextArea(
-                value = state.aboutMeField,
-                onValueChange = { /* TODO */ }
-            )
-        }
+        Spacer(Modifier.padding(ABOUT_ME_MIDDLE_SPACER))
+        Text(
+            text = stringResource(R.string.about_me_tab_label),
+            style = Theme.typography.body7Alt,
+            color = Theme.colors.black900,
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Spacer(Modifier.padding(PURPLE_AREA_TEXT_SPACER))
+
+        DefaultTextField(
+            value = state.aboutMeField,
+            placeholder = stringResource(R.string.about_me_tab_placeholder),
+            onValueChange = { /* TODO */ },
+            onExpandedChange = { /* TODO */ },
+            singleLine = false,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(160.dp),
+        )
     }
-}
-
-@Composable
-fun PurpleTextArea(
-    value: String,
-    onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    OutlinedTextField(
-        value = value,
-        onValueChange = onValueChange,
-        modifier = modifier
-            .fillMaxWidth()
-            .height(160.dp),
-        shape = RoundedCornerShape(20.dp),
-        placeholder = {
-             Text("Android разработчик с фокусом на Compose и архитектуру.")
-        },
-        maxLines = Int.MAX_VALUE,
-        singleLine = false,
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = TextInputColorsDefaults.activeBorder(),
-            unfocusedBorderColor = TextInputColorsDefaults.defaultsBorder(),
-            disabledBorderColor = TextInputColorsDefaults.defaultsBorder(),
-            cursorColor = TextInputColorsDefaults.activeBorder()
-        ),
-        textStyle = Theme.typography.body3
-    )
 }
 
 @Preview(showBackground = true)
