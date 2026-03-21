3612e8f95a43834dacfed3642edbbd5746137b3f
Yrun00 <danil.budarenkov@gmail.com>
2026-03-21 07:28:08 +0400

    delete showDialog command

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenCommand.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenCommand.kt
@@ -3,7 +3,6 @@ package ru.yeahub.profile_edit.impl.presentation.intents
 import ru.yeahub.core_utils.common.TextOrResource
 
 sealed interface ProfileEditScreenCommand {
-    data object ShowUnsavedChangesDialog : ProfileEditScreenCommand
     data object ShowPhotoPicker : ProfileEditScreenCommand
     data class ShowError(val message: TextOrResource) : ProfileEditScreenCommand
 }
