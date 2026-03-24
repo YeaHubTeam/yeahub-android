0caaa06f1bf4049510c5e98409019a4e231bf755
Artemiy <palach123456@gmail.com>
2026-03-24 15:56:39 +0300

    ANDR-63: - Изменил на в маппере скиллы также на persistentlist

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuizResult/InterviewQuizResultScreenMapper.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuizResult/InterviewQuizResultScreenMapper.kt
@@ -1,6 +1,7 @@
 package ru.yeahub.interview_trainer.impl.interviewQuizResult
 
 import InterviewQuizResultState
+import kotlinx.collections.immutable.persistentListOf
 
 private const val DEFAULT_PERCENTAGE = 0.75f
 private const val DEFAULT_TOTAL_QUESTIONS = 20
@@ -18,7 +19,7 @@ class InterviewQuizResultScreenMapper {
         newQuestions = DEFAULT_NEW_QUESTIONS,
         inProgress = DEFAULT_IN_PROGRESS,
         studied = DEFAULT_STUDIED,
-        skills = listOf(
+        skills = persistentListOf(
             InterviewQuizResultState.Loaded.VoSkillStat("HTML", DEFAULT_SKILL_SCORE, DEFAULT_SKILL_MAX),
             InterviewQuizResultState.Loaded.VoSkillStat("CSS", DEFAULT_SKILL_SCORE, DEFAULT_SKILL_MAX),
             InterviewQuizResultState.Loaded.VoSkillStat("JavaScript", DEFAULT_SKILL_SCORE, DEFAULT_SKILL_MAX),
@@ -26,7 +27,7 @@ class InterviewQuizResultScreenMapper {
             InterviewQuizResultState.Loaded.VoSkillStat("PHP", DEFAULT_SKILL_SCORE, DEFAULT_SKILL_MAX),
             InterviewQuizResultState.Loaded.VoSkillStat("JavaScript", DEFAULT_SKILL_SCORE, DEFAULT_SKILL_MAX)
         ),
-        questions = listOf(
+        questions = persistentListOf(
             InterviewQuizResultState.Loaded.VoQuestionResult("Что такое Virtual DOM, и как он работает?", false),
             InterviewQuizResultState.Loaded.VoQuestionResult("Что такое Virtual DOM, и как он работает?", true),
             InterviewQuizResultState.Loaded.VoQuestionResult("Что такое Virtual DOM, и как он работает?", true),
