f6765e02ea7cd6c16b846a70c4243c2af6f307ff
Artemiy <palach123456@gmail.com>
2026-03-24 21:53:11 +0300

    ANDR-63: Изменил ViewModel на BaseViewModel

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuizResult/InterviewQuizResultViewModel.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuizResult/InterviewQuizResultViewModel.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
+import ru.yeahub.core_utils.BaseViewModel
 import ru.yeahub.interview_trainer.impl.interviewQuizResult.InterviewQuizResultEvent
 import ru.yeahub.interview_trainer.impl.interviewQuizResult.InterviewQuizResultScreenMapper
 
@@ -11,7 +12,7 @@ private const val STOP_TIMEOUT_MILLIS = 5000L
 
 class InterviewQuizResultViewModel(
     private val mapper: InterviewQuizResultScreenMapper
-) : ViewModel() {
+) : BaseViewModel() {
 
     val state: StateFlow<InterviewQuizResultState> =
         flow {
