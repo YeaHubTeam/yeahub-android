783c5edc9a6eff804599cd70850e57ce322e137a
PanMobile <chronosroseviknr@gmail.com>
2026-03-25 21:05:14 +0300

    ANDR-94: Создание провайдер-функции для динамического превью и документации к ней

--- core/ui/src/main/java/ru/yeahub/core_ui/example/dynamicPreview/DynamicPreview.kt
+++ core/ui/src/main/java/ru/yeahub/core_ui/example/dynamicPreview/DynamicPreview.kt
@@ -1,8 +1,13 @@
 package ru.yeahub.core_ui.example.dynamicPreview
 
 import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-
+import org.koin.compose.KoinIsolatedContext
+import org.koin.dsl.ModuleDeclaration
+import org.koin.dsl.koinApplication
+import org.koin.dsl.module
 
 @Preview(
     name = "Dynamic normal screen (phone) preview",
@@ -26,4 +31,54 @@ import androidx.compose.ui.tooling.preview.Preview
 )
 annotation class DynamicPreview
 
+/**
+ * Провайдер для создания динамических превью с реальными зависимостями
+ *
+ * Динамическое превью позволяет запустить экран с настоящей ViewModel'ью и DI-зависимостями
+ * для проверки изменения параметров Loaded стейта (Loading и Error посмотреть можно, а зачем?)
+ *
+ * Каждый вариант превью получает изолированный Koin-контекст через [KoinIsolatedContext],
+ * поэтому несколько превью одного экрана (телефон + планшет) не конфликтуют между собой.
+ *
+ * Для полноценной проверки активируйте Interactive Mode:
+ * нажмите на иконку трёх точек (󰇙) в тулбаре над превью и выберете Start Interactive Mode
+ *
+ * **Ограничение:** ViewModel не должна выполнять реальные IO-операции, т.к. превью работает
+ * без сети и без БД. Мокайте UseCase'ы с заранее подготовленными данными.
+ * Подробнее об ограничениях превью:
+ * [Compose Previews](https://developer.android.com/develop/ui/compose/tooling/previews)
+ *
+ * @param moduleDeclaration лямбда, задающая Koin-модуль с зависимостями экрана (UseCase, ViewModel)
+ * @param content Composable-контент для отображения в превью (не обязательно весь экран)
+ */
+@Composable
+fun ProvideDynamicPreview(
+    moduleDeclaration: ModuleDeclaration,
+    content: @Composable () -> Unit,
+) {
+    val koinApp = remember {
+        koinApplication {
+            modules(module(moduleDeclaration = moduleDeclaration))
+        }
+    }
 
+    KoinIsolatedContext(context = koinApp) {
+        ProvidePreviewCompositionLocals(content)
+    }
+}
+
+//@Composable
+//inline fun <reified BVM : BaseViewModel> ProvideDynamicPreview(
+//    noinline moduleDeclaration: ModuleDeclaration,
+//    noinline content: @Composable (BVM) -> Unit,
+//) {
+//    val koinApp = remember {
+//        koinApplication {
+//            modules(module(moduleDeclaration = moduleDeclaration))
+//        }
+//    }
+//
+//    KoinIsolatedContext(context = koinApp) {
+//        ProvidePreviewCompositionLocals { content(koinViewModel<BVM>()) }
+//    }
+//}
