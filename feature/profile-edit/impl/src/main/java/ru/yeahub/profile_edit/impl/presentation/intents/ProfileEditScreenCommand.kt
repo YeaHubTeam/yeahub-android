041496406d214f91d742cb5b77cd1c207a650be9
Yrun00 <danil.budarenkov@gmail.com>
2026-03-30 00:35:05 +0400

    made res/comm/event internal, add additional commands

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenCommand.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/presentation/intents/ProfileEditScreenCommand.kt
@@ -2,7 +2,10 @@ package ru.yeahub.profile_edit.impl.presentation.intents
 
 import ru.yeahub.core_utils.common.TextOrResource
 
-sealed interface ProfileEditScreenCommand {
+internal sealed interface ProfileEditScreenCommand {
     data object ShowPhotoPicker : ProfileEditScreenCommand
     data class ShowError(val message: TextOrResource) : ProfileEditScreenCommand
+    data object NavigateBack : ProfileEditScreenCommand
+    data object NavigateToProfile : ProfileEditScreenCommand
+    data object ShowCannotChangeSpecializationToast : ProfileEditScreenCommand
 }
