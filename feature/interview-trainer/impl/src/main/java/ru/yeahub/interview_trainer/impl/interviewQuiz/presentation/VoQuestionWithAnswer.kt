c4245a99f892f5ef79c72cccae6a249e1afbeab6
Deyryl <snegurivan.of@mail.ru>
2026-03-30 15:35:59 +0300

    ANDR-72: использование Parcelize для передачи между экранами

--- feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuiz/presentation/VoQuestionWithAnswer.kt
+++ feature/interview-trainer/impl/src/main/java/ru/yeahub/interview_trainer/impl/interviewQuiz/presentation/VoQuestionWithAnswer.kt
@@ -1,16 +1,20 @@
 package ru.yeahub.interview_trainer.impl.interviewQuiz.presentation
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.parcelize.Parcelize
 
 @Immutable
+@Parcelize
 data class VoQuestionWithAnswer(
     val id: Long,
     val title: String,
     val shortAnswer: String,
     val userAnswer: QuizAnswerResult
-)
+) : Parcelable
 
-enum class QuizAnswerResult {
+@Parcelize
+enum class QuizAnswerResult : Parcelable {
     KNOWN,
     UNKNOWN
 }
