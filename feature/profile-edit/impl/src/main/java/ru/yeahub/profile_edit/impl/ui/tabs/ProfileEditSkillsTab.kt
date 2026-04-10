751d04bd27b2e6f6c9f7dd818fcb74dc091f5d40
Yrun00 <danil.budarenkov@gmail.com>
2026-04-10 22:14:54 +0400

    add .verticalScroll to SkillsContent

--- feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/ui/tabs/ProfileEditSkillsTab.kt
+++ feature/profile-edit/impl/src/main/java/ru/yeahub/profile_edit/impl/ui/tabs/ProfileEditSkillsTab.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -46,7 +48,8 @@ internal fun SkillsContent(
         modifier = Modifier
             .fillMaxWidth()
             .background(Theme.colors.white900)
-            .padding(top = TAB_CONTENT_TOP_PADDING),
+            .padding(top = TAB_CONTENT_TOP_PADDING)
+            .verticalScroll(rememberScrollState()),
     ) {
         SectionHeader(
             title = stringResource(R.string.skills_tab),
